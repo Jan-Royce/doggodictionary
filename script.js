371eea1ef74b210ceed9373f5846401696813c42
Jan Royce Maceren <janroycemaceren@gmail.com>
2019-05-27 22:55:00 +0800

    doing the nice styling

--- script.js
+++ script.js
@@ -8,23 +8,27 @@ function displayDoggoData(d)
   {
     let entry = document.createElement('div');
     entry.className = "entry";
-    let word = document.createElement('span');
+    let word = document.createElement('div');
     word.className = "entry-word";
     word.textContent = d[i].word;
-    let pos = document.createElement('p');
+    let pos = document.createElement('span');
     pos.className = "entry-pos";
     pos.textContent = `(${d[i].pos})`;
     let def = document.createElement('p');
     def.className = "entry-definition";
     def.textContent = d[i].definition;
     let img = document.createElement('img');
-    img.className = "entry-image";
     img.src = d[i].image;
+    let img_container = document.createElement('div');
+    img_container.className = "entry-image";
 
+    img_container.appendChild(img);
+    word.appendChild(pos);
+
+    entry.appendChild(img_container);
     entry.appendChild(word);
-    entry.appendChild(pos);
     entry.appendChild(def);
-    entry.appendChild(img);
+
     container.appendChild(entry);
   }
 }
