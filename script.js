c01c50dd8f2777877e9bc323022a784422e6168d
Jan Royce Maceren <janroycemaceren@gmail.com>
2019-05-27 18:40:26 +0800

    more json...

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 console.log("parsing json");
 let doggo_json = "assets/doggo.json";
 
+function displayDoggoData(d)
+{
+  for(i=0;i<d.length;i++)
+  {
+
+  }
+}
+
 async function getDoggoData()
 {
   const response = await fetch(doggo_json);
@@ -8,6 +16,7 @@ async function getDoggoData()
 
   console.log(data);
   console.log(data.length);
+  console.log(data[0]);
 }
 
 getDoggoData();
